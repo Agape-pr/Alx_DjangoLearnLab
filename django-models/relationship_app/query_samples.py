f1290c0e0990c1c6782d325fe4c99c2c2dea1c89
Agape-pr <mugishaagapelearning@gmail.com>
2024-11-09 16:36:46 +0800

    Update query_samples.py

--- django-models/relationship_app/query_samples.py
+++ django-models/relationship_app/query_samples.py
@@ -1,7 +1,9 @@
 from .models import Author, Library
 
 
-book = Author.objects.filter(name = 'agape')
+objects.filter(author=author)
+Author.objects.get(name=author_name)
+
 books = Library.objects.all()
 Library.objects.get(name=library_name)
 books.all()

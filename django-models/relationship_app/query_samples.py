a3dcaf273426d9d2b30f73d9585145d72fb90669
Agape Mugisha <mugishaagape1@gmail.com>
2024-11-10 05:50:37 +0800

    added

--- django-models/relationship_app/query_samples.py
+++ django-models/relationship_app/query_samples.py
@@ -1,11 +1,6 @@
 from .models import Author, Library
 
 
-<<<<<<< HEAD
-book = Author.objects.filter(name = 'agape')
-books = Library.objects.all()
-libraryp = Library.objects.get(name = 'name')
-=======
 objects.filter(author=author)
 Author.objects.get(name=author_name)
 
@@ -15,4 +10,3 @@ Library.objects.get(name=library_name)
 books.all()
 
 
->>>>>>> 2074e764f1ace93defdcc7cca6a2981717240405

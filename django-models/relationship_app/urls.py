4c14883d03a19add5dd9c77ee1fd786af73940e6
Agape Mugisha <mugishaagape1@gmail.com>
2024-11-10 07:18:10 +0800

    gg

--- django-models/relationship_app/urls.py
+++ django-models/relationship_app/urls.py
@@ -10,7 +10,7 @@ urlpatterns = [
     path('library/', LibraryDetailView.as_view(), name = 'list' ),
     path('register/', views.register),
     path('login/', LoginView.as_view(template_name='registration/login.html'), name='login'),
-    path('logout/', LogoutView.as_view(), name='logout')
+    path('logout/', LogoutView.as_view(template_name='logout.html'), name='logout')
 ]
 
 

fc9772cfc9fe0a1d79c005fe9541a2bff8e8c562
Agape Mugisha <mugishaagape1@gmail.com>
2024-11-10 08:33:12 +0800

    j

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -10,6 +10,9 @@ from django.urls import reverse_lazy
 from django.views.generic import CreateView
 from django.contrib.auth import login
 
+from django.contrib.auth.decorators import user_passes_test
+from .models import UserProfile
+
 def viewmodel(request):
     books = Book.objects.all()
     books_list= "\n".join([f"{book.title} by {book.author.name}"for book in books])  
@@ -36,3 +39,18 @@ class SignUpView(CreateView):
     success_url = reverse_lazy('login')
     template_name = 'relationship_app/register.html'
 
+
+
+
+def admin_view(request):
+    return render(request, 'admin_view.html')
+
+# Librarian view
+@user_passes_test(lambda u: u.userprofile.role == 'Librarian')
+def librarian_view(request):
+    return render(request, 'librarian_view.html')
+
+# Member view
+@user_passes_test(lambda u: u.userprofile.role == 'Member')
+def member_view(request):
+    return render(request, 'member_view.html')

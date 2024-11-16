0527d0ed07b963097330ca1029bd724e8f180c66
Agape Mugisha <mugishaagape1@gmail.com>
2024-11-16 16:57:02 +0800

    updatd views and url file

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -2,22 +2,17 @@ from django.shortcuts import render
 
 from django.views.generic.detail import DetailView
 from django.http import HttpRequest
-from .models import Book
-from .models import Library
-from django.contrib.auth.forms import UserCreationForm
+from .models import Book, Library, UserProfile
 from django.contrib.auth.forms import UserCreationForm
 from django.urls import reverse_lazy
 from django.views.generic import CreateView
 from django.contrib.auth import login
-
-from django.contrib.auth.decorators import user_passes_test
-from .models import UserProfile
 from django.contrib.auth.decorators import user_passes_test
 
 
 
-from django.shortcuts import render
-from django.http import HttpResponseForbidden
+
+
 def viewmodel(request):
     books = Book.objects.all()
     books_list= "\n".join([f"{book.title} by {book.author.name}"for book in books])  
@@ -54,11 +49,12 @@ class SignUpView(CreateView):
 #   return render(request, 'admin_view.html')
 def is_admin(user):
     return getattr(user, 'userprofile', None) and user.userprofile.role == 'Admin'
-@user_passes_test(is_admin)
 
+@user_passes_test(is_admin)
 def admin_view(request):
     # Your view logic here
     return render(request, 'admin_dashboard.html')
+
 # Librarian view that only users with the 'Librarian' role can access
 @user_passes_test(lambda u: u.userprofile.role == 'Librarian')
 def librarian_view(request):

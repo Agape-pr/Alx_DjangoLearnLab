337054b6ce327e95a0d1004ed5f3a24c44dbd228
Agape Mugisha <mugishaagape1@gmail.com>
2024-11-16 17:00:31 +0800

    gg

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -51,7 +51,7 @@ def is_admin(user):
     return getattr(user, 'userprofile', None) and user.userprofile.role == 'Admin'
 
 @user_passes_test(is_admin)
-def admin_view(request):
+def Admin_view(request):
     # Your view logic here
     return render(request, 'admin_dashboard.html')
 

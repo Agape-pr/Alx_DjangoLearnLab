1a74b4431c8098e0d861089bb390737c1e0d45ea
Agape Mugisha <mugishaagape1@gmail.com>
2024-11-22 14:19:05 +0800

    updated the models .py of bookshelf to handle user and supper user  creation

--- advanced_features_and_security/LibraryProject/bookshelf/models.py
+++ advanced_features_and_security/LibraryProject/bookshelf/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from  django.contrib.auth.models import BaseUserManager
+from django.contrib.auth.models import AbstractUser, BaseUserManager
 class Book(models.Model):
     title = models.CharField(max_length=200)
     author = models.CharField(max_length=100)
@@ -11,3 +13,23 @@ class CustomUser(AbstractUser):
     date_of_birth = models.DateField()
     profile_photo = models.ImageField(upload_to='profile_photos/')
 
+
+class UserManager(BaseUserManager):
+    def create_user(self, username, password):
+        if not username :
+            raise ValueError("Username is required")
+        if not password :
+            raise ValueError("password is required")
+        user = self.model(username = username)
+        user.set_passwod(password)
+        user.save()
+        return user
+    def create_superuser(self,username, password):
+        if not username :
+            raise ValueError("username required")
+        if not password :
+            raise ValueError("Password is required")
+        user = self.create_user(username, password)
+        user.is_staff = True
+        user.is_superuser = True
+        user.save()

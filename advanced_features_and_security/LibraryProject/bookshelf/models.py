7c8e918ef45eb42728011fc4bea201b4335791b9
Agape Mugisha <mugishaagape1@gmail.com>
2024-11-22 13:59:02 +0800

    tpdated the bookshelf app's models.py

--- advanced_features_and_security/LibraryProject/bookshelf/models.py
+++ advanced_features_and_security/LibraryProject/bookshelf/models.py
@@ -1,7 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 class Book(models.Model):
     title = models.CharField(max_length=200)
     author = models.CharField(max_length=100)
     publication_year = models.IntegerField()
     
 # Create your models here.
+
+class CustomUser(AbstractUser):
+    date_of_birth = models.DateField()
+    profile_photo = models.ImageField(upload_to='profile_photos/')
+

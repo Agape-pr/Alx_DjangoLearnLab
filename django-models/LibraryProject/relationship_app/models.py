fa1844f1141ae2c102808eea2b367358697bda9d
Agape Mugisha <mugishaagape1@gmail.com>
2024-11-24 00:32:41 +0800

    added the file

--- django-models/LibraryProject/relationship_app/models.py
+++ django-models/LibraryProject/relationship_app/models.py
@@ -60,26 +60,8 @@ class UserProfile(models.Model):
     
    
 
-# relationship_app/models.py
-from django.db import models
 
-class Book(models.Model):
-    title = models.CharField(max_length=100)
-    author = models.CharField(max_length=100)
-    publication_year = models.IntegerField()
-
-    class Meta:
-        permissions = [
-            ("can_add_book", "Can add book"),
-            ("can_change_book", "Can change book"),
-            ("can_delete_book", "Can delete book"),
-        ]
     
-    
-
-
-
-
 
 
 

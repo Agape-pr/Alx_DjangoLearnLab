f9ffad48fecf1f24f2a08ebee9c5199ea12fcb9b
Agape Mugisha <mugishaagape1@gmail.com>
2024-12-02 17:13:48 +0800

    debuged naming errors

--- advanced-api-project/api/serializers.py
+++ advanced-api-project/api/serializers.py
@@ -2,25 +2,27 @@ from rest_framework import serializers
 from .models import Author, Book
 from datetime import datetime
 
-class Authorserializer(serializers.ModelSerializer):
+class AuthorSerializer(serializers.ModelSerializer):
     
     class Meta:
         
         model = Author
         fields = ['name']
 
-class Bookserializer(serializers.ModelSerializer):
+
     #serialization for the relationship btn Book and Author models, named valiable , 
     # equate to the serializer class of the author class , define its two variable "many and read_only to True"
-    author_name = Authorserializer(many = True , read_only = True)
+class BookSerializer(serializers.ModelSerializer):
+        
+    author_name = AuthorSerializer(many = True , read_only = True)
     class Meta:
         model = Book
         fields = '__all__' 
-    def validate(self,data):
-       # publication_yeaar < date.now or  publication_year = date.now
-        if not (data['publication_year'] < datetime.now().year or data['publication_year'] == datetime.now().year ):
-            raise serializers.ValidationError("Year should be current year")
-        return data
+        def validate(self,data):
+        # publication_yeaar < date.now or  publication_year = date.now
+            if not (data['publication_year'] < datetime.now().year or data['publication_year'] == datetime.now().year ):
+                raise serializers.ValidationError("Year should be current year")
+            return data
             
         
     
